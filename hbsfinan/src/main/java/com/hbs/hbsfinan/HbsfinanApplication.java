191ee5449e169b012f5b32222f8e48268c1cddf6
JoevitorSantana <joevitoralvessantana@gmail.com>
2025-04-18 19:03:59 -0300

    Estrutura inicial do projeto

---
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/HbsfinanApplication.java
@@ -0,0 +1,13 @@
+package com.hbs.hbsfinan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbsfinanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbsfinanApplication.class, args);
+    }
+
+}
---
+++ hbsfinan/src/test/java/com/hbs/hbsfinan/HbsfinanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbs.hbsfinan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbsfinanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
