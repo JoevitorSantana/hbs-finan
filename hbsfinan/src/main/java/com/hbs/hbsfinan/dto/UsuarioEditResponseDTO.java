cfd3092b850f8015a2322cc305b7c77f970344c3
JoevitorSantana <joevitoralvessantana@gmail.com>
2025-04-30 00:35:37 -0300

    Alterando formato de respostas Usuários

---
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/dto/UsuarioEditResponseDTO.java
@@ -0,0 +1,4 @@
+package com.hbs.hbsfinan.dto;
+
+public record UsuarioEditResponseDTO(int id, String nome, String ultimoNome, String email, String senha, String role) {
+}
---
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/dto/UsuarioResponseDTO.java
@@ -0,0 +1,4 @@
+package com.hbs.hbsfinan.dto;
+
+public record UsuarioResponseDTO(int id, String nome, String ultimoNome, String email) {
+}
