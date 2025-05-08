4790a765ae5f85e55a11720b168d1b0f11a8c27d
Maria Isabelly Andrade <andradeisabellypessoal@gmail.com>
2025-05-08 15:40:44 -0300

    Apoiador funcionalidade parcial

---
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/repository/interfaces/IApoiadorRepository.java
@@ -0,0 +1,16 @@
+package com.hbs.hbsfinan.repository.interfaces;
+
+import com.hbs.hbsfinan.model.Apoiador;
+import com.hbs.hbsfinan.model.Funcionario;
+
+import java.util.List;
+
+public interface IApoiadorRepository {
+
+    void save(Apoiador apoiador);
+    Apoiador findById(int id);
+    List<Apoiador> findAll();
+    void delete(int id);
+    void update(Apoiador apoiador);
+    Apoiador findByEmail(String email);
+}
