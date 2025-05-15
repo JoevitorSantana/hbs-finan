d3d940e425082c9f1fdfc33bfef3ee217e6b295c
Ana Caroline <carolineana2003142@gmail.com>
2025-05-15 19:42:45 -0300

    crud funcionario

--- hbsfinan/src/main/java/com/hbs/hbsfinan/repository/implementation/ApoiadorRepository.java
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/repository/implementation/ApoiadorRepository.java
@@ -1,67 +1,67 @@
-//package com.hbs.hbsfinan.repository.implementation;
-//
-//import com.hbs.hbsfinan.model.Apoiador;
-//import com.hbs.hbsfinan.model.Funcionario;
-//import com.hbs.hbsfinan.repository.interfaces.IApoiadorRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.RowMapper;
-//
-//import java.util.List;
-//
-//public class ApoiadorRepository implements IApoiadorRepository {
-//    @Autowired
-//    private JdbcTemplate dbConn;
-//
-//
-//    //conferir no banco
-//    private RowMapper<Apoiador> rowMapper = (rs, rowNum) -> {
-//       Apoiador apoiador = new Apoiador();
-//        apoiador.setId(rs.getInt("id"));
-//        apoiador.setCpf(rs.getString("cpf"));
-//        apoiador.setId(rs.getInt("id"));
-//        apoiador.setCpf(rs.getString("cpf"));
-//        apoiador.setId(rs.getInt("id"));
-//        apoiador.setCpf(rs.getString("cpf"));
-//        apoiador.setId(rs.getInt("id"));
-//        apoiador.setCpf(rs.getString("cpf"));
-//        return apoiador;
-//    };
-//
-//
-//
-//    @Override
-//    public void save(Apoiador apoiador) {
-//        dbConn.update("INSERT INTO apoiador (id, nome, endereco, sexo, telefone, cpf, email , data_nascimento) VALUES (?,?,?,?,?,?,?,?)", apoiador.getId(), apoiador.getNome(), apoiador.getEndereco(),apoiador.getSexo(), apoiador.getFone(), apoiador.getCpf(),apoiador.getEmail(),apoiador.getCpf());
-//    }
-//
-//    @Override
-//    public Apoiador findById(int id) {
-//        return dbConn.queryForObject("SELECT * FROM apoiador WHERE id = ?", rowMapper, id);
-//    }
-//
-//    @Override
-//    public List<Apoiador> findAll() {
-//        return dbConn.query("SELECT * FROM apoiador", rowMapper);
-//    }
-//
-//    @Override
-//    public void delete(int id) {
-//
-//    }
-//
-//    @Override
-//    public void update(Apoiador apoiador) {
-//        dbConn.update("UPDATE apoiador SET nome = ?, endereco = ?, sexo = ?, telefone=?, cpf=? WHERE id = ?",apoiador.getNome(), apoiador.getEndereco(),apoiador.getSexo(), apoiador.getFone(), apoiador.getCpf(),apoiador.getEmail(),apoiador.getCpf(),apoiador.getId());
-//    }
-//
-//    @Override
-//    public Apoiador findByEmail(String email) {
-//        try {
-//            return dbConn.queryForObject("SELECT * FROM apoiador WHERE email = ?", rowMapper, email);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//}
+package com.hbs.hbsfinan.repository.implementation;
+
+import com.hbs.hbsfinan.model.Apoiador;
+import com.hbs.hbsfinan.model.Funcionario;
+import com.hbs.hbsfinan.repository.interfaces.IApoiadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
+public class ApoiadorRepository implements IApoiadorRepository {
+    @Autowired
+    private JdbcTemplate dbConn;
+
+
+    //conferir no banco
+    private RowMapper<Apoiador> rowMapper = (rs, rowNum) -> {
+       Apoiador apoiador = new Apoiador();
+        apoiador.setId(rs.getInt("id"));
+        apoiador.setCpf(rs.getString("cpf"));
+        apoiador.setId(rs.getInt("id"));
+        apoiador.setCpf(rs.getString("cpf"));
+        apoiador.setId(rs.getInt("id"));
+        apoiador.setCpf(rs.getString("cpf"));
+        apoiador.setId(rs.getInt("id"));
+        apoiador.setCpf(rs.getString("cpf"));
+        return apoiador;
+    };
+
+
+
+    @Override
+    public void save(Apoiador apoiador) {
+        dbConn.update("INSERT INTO apoiador (id, nome, endereco, sexo, telefone, cpf, email , data_nascimento) VALUES (?,?,?,?,?,?,?,?)", apoiador.getId(), apoiador.getNome(), apoiador.getEndereco(),apoiador.getSexo(), apoiador.getFone(), apoiador.getCpf(),apoiador.getEmail(),apoiador.getCpf());
+    }
+
+    @Override
+    public Apoiador findById(int id) {
+        return dbConn.queryForObject("SELECT * FROM apoiador WHERE id = ?", rowMapper, id);
+    }
+
+    @Override
+    public List<Apoiador> findAll() {
+        return dbConn.query("SELECT * FROM apoiador", rowMapper);
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(Apoiador apoiador) {
+        dbConn.update("UPDATE apoiador SET nome = ?, endereco = ?, sexo = ?, telefone=?, cpf=? WHERE id = ?",apoiador.getNome(), apoiador.getEndereco(),apoiador.getSexo(), apoiador.getFone(), apoiador.getCpf(),apoiador.getEmail(),apoiador.getCpf(),apoiador.getId());
+    }
+
+    @Override
+    public Apoiador findByEmail(String email) {
+        try {
+            return dbConn.queryForObject("SELECT * FROM apoiador WHERE email = ?", rowMapper, email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- hbsfinan/src/main/java/com/hbs/hbsfinan/service/ApoiadorService.java
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/service/ApoiadorService.java
@@ -1,37 +1,37 @@
-//package com.hbs.hbsfinan.service;
-//
-//import com.hbs.hbsfinan.model.Apoiador;
-//import com.hbs.hbsfinan.model.Funcionario;
-//import com.hbs.hbsfinan.repository.implementation.ApoiadorRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class ApoiadorService {
-//
-//    @Autowired
-//    private ApoiadorRepository apoiadorRepository;
-//
-//    public void save(Apoiador apoiador) {
-//        apoiadorRepository.save(apoiador);
-//    }
-//
-//    public List<Apoiador> findAll() {
-//        return apoiadorRepository.findAll();
-//    }
-//
-//    public void delete(int id) {
-//        apoiadorRepository.delete(id);
-//    }
-//
-//    public void update(Apoiador apoiador) {
-//        apoiadorRepository.update(apoiador);
-//    }
-//
-//    public Apoiador findById(int id) {
-//        return apoiadorRepository.findById(id);
-//    }
-//
-//}
+package com.hbs.hbsfinan.service;
+
+import com.hbs.hbsfinan.model.Apoiador;
+import com.hbs.hbsfinan.model.Funcionario;
+import com.hbs.hbsfinan.repository.implementation.ApoiadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ApoiadorService {
+
+    @Autowired
+    private ApoiadorRepository apoiadorRepository;
+
+    public void save(Apoiador apoiador) {
+        apoiadorRepository.save(apoiador);
+    }
+
+    public List<Apoiador> findAll() {
+        return apoiadorRepository.findAll();
+    }
+
+    public void delete(int id) {
+        apoiadorRepository.delete(id);
+    }
+
+    public void update(Apoiador apoiador) {
+        apoiadorRepository.update(apoiador);
+    }
+
+    public Apoiador findById(int id) {
+        return apoiadorRepository.findById(id);
+    }
+
+}
