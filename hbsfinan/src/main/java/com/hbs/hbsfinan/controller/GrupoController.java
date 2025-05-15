c95b1fa669d5c112eef7830fd5d9b5c0ca5ef6b1
PedroManzano1 <pedromanz2812@gmail.com>
2025-05-15 11:24:18 -0300

    CRUD GRUPOS(NAO COMPLETO)

--- hbsfinan/src/main/java/com/hbs/hbsfinan/controller/GrupoController.java
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/controller/GrupoController.java
@@ -1,13 +1,46 @@
 package com.hbs.hbsfinan.controller;
 
+import com.hbs.hbsfinan.dto.RestResponseMessage;
+import com.hbs.hbsfinan.model.Grupo;
 import com.hbs.hbsfinan.service.GrupoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/grupo")
+@RequestMapping("/grupos")
 public class GrupoController {
     @Autowired
     GrupoService grupoService;
+
+    @PostMapping("/novo")
+    public ResponseEntity save(@Valid @RequestBody Grupo grupo){
+        try
+        {
+            grupoService.save(grupo);
+            RestResponseMessage message = new RestResponseMessage(HttpStatus.CREATED, "Grupo inserido com sucesso!");
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Erro ao salvar grupo: " + e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Grupo>> findAll(){
+        try
+        {
+            return ResponseEntity.ok(grupoService.findAll());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }return ResponseEntity.badRequest().build();
+    }
 }
