370745e42d4b179177e5009332d8c8e7bafd0e8d
PedroManzano1 <pedromanz2812@gmail.com>
2025-05-22 00:09:01 -0300

    Começo classe evento

---
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/repository/interfaces/IEventoRepository.java
@@ -0,0 +1,12 @@
+package com.hbs.hbsfinan.repository.interfaces;
+import com.hbs.hbsfinan.model.Evento;
+import java.util.List;
+
+public interface IEventoRepository {
+    void save(Evento evento);
+    void delete(int id);
+    void update(Evento evento);
+    Evento findById(int id);
+    Evento findByNome(String nome);
+    List<Evento> findAll();
+}
