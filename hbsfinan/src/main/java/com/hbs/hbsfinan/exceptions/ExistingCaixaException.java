40388c72302b0c308e908be7fd82a1dbefbf1fbf
Joevitor Alves Santana <joevitoralvessantana@gmail.com>
2025-06-05 23:37:38 -0300

    Delete hbsfinan/src/main/java/com/hbs/hbsfinan/exceptions/ExistingCaixaException.java

--- hbsfinan/src/main/java/com/hbs/hbsfinan/exceptions/ExistingCaixaException.java
+++
@@ -1,7 +0,0 @@
-package com.hbs.hbsfinan.exceptions;
-
-public class ExistingCaixaException extends RuntimeException {
-    public ExistingCaixaException(String message) {
-        super(message);
-    }
-}
