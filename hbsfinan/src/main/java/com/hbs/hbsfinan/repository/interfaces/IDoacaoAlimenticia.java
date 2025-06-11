d8ecc7663729207dd7ad75e83b247ef61dfb221d
PedroManzano1 <pedromanz2812@gmail.com>
2025-06-11 18:51:18 -0300

    correção de erro

--- hbsfinan/src/main/java/com/hbs/hbsfinan/repository/interfaces/IDoacaoAlimenticia.java
+++ hbsfinan/src/main/java/com/hbs/hbsfinan/repository/interfaces/IDoacaoAlimenticia.java
@@ -1,19 +1,19 @@
-//package com.hbs.hbsfinan.repository.interfaces;
-//
-//import com.hbs.hbsfinan.dto.ApoiadorDTO;
-//import com.hbs.hbsfinan.model.Apoiador;
-//import com.hbs.hbsfinan.model.DoacaoAlimenticia;
-//
-//
-//
-//
-//
-//import java.util.List;
-//
-//public interface IDoacaoAlimenticia {
-//    void save(DoacaoAlimenticia doacaoAlimenticia);
-//    DoacaoAlimenticia findById(int id);
-//    List<DoacaoAlimenticia> findAll();
-//    void delete(int id);
-//    void update(DoacaoAlimenticia doacaoAlimenticia);
-//}
+package com.hbs.hbsfinan.repository.interfaces;
+
+import com.hbs.hbsfinan.model.DoacaoProduto;
+import java.util.List;
+
+public interface IDoacaoAlimenticia {
+
+    // Salvar nova doação, incluindo seus itens
+    void save(DoacaoProduto doacaoAlimenticia);
+
+    // Buscar doação pelo ID, incluindo itens associados
+    DoacaoProduto findById(int id);
+
+    // Listar todas as doações com seus itens
+    List<DoacaoProduto> findAll();
+
+    // Deletar doação pelo ID
+    void delete(int id);
+}
